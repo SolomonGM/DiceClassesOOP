9e12db7d5971b4573829f10293d65b05b65e69ab
Solomon Oduyebo <124704556+SolomonGM@users.noreply.github.com>
2024-04-29 20:14:31 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+using Three_Class_Task;
+
+namespace Three_Dice_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Created objects of the Game and Testing classes
+            SevenOut sevenOut = new SevenOut();
+            sevenOut.GameMenu();
+        }
+    }
+}
